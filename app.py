fb2025dad876157ce4ed9a81f971148172425d77
deanb <deanalexander.buno@my.jcu.edu.au>
2022-01-19 12:58:13 +0800

    Modify function `f` in `app.py` to convert temperature from degree Celcius to degree Fahrenheit (if it works).

--- app.py
+++ app.py
@@ -14,19 +14,25 @@ PORT_NUMBER = 8500
 
 @app.route('/')
 def hello_world():
+    """ Standard Page """
     return '<h1>Hello World! :)</h1>'
 
 
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
+    """ Greeting Page """
     return f"Hello {name}"
 
 
 @app.route('/f')
 @app.route('/f/<temperature_in_celcius>')
 def f(temperature_in_celcius=0):
-    pass
+    """ Temperature Page """
+    try:
+        return str((float(temperature_in_celcius) * (9/5)) + 32)
+    except ValueError:
+        return "This value cannot be converted!"
 
 
 if __name__ == '__main__':
